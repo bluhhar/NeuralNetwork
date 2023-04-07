f2a12cb5abfdd853df08e9ac3bfb9879a8ba8c7e
bluhhar <bluhhar@DESKTOP-J8AJ9KC>
2023-04-07 16:48:52 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetwork.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace NeuralNetwork.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ NeuralNetwork/Classes/Neuron.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.Classes
+{
+    public class Neuron
+    {
+        private double[] weights;
+        private double bias;
+
+        public Neuron(int inputCount)
+        {
+            weights = new double[inputCount];
+            RandomizeWeights();
+            bias = 0;
+        }
+
+        public double FeedForward(double[] inputs)
+        {
+            double sum = 0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                sum += inputs[i] * weights[i];
+            }
+            sum += bias;
+            return Sigmoid(sum);
+        }
+
+        private double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+
+        private void RandomizeWeights()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = rand.NextDouble() * 2 - 1;
+            }
+        }
+    }
+
+}
